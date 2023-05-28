b27c19aed3c3938f65b174fa870c996a718ac5f2
witch-factory <soakdma37@gmail.com>
2023-05-28 23:48:15 +0900

    footer css 추가

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import blogConfig from 'blog-config';
+
+import styles from './styles.module.css';
 
 function Footer() {
   return (
-    <footer>
-    © {blogConfig.name}, Built with NextJS, 2023
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+      © {blogConfig.name}, Built with NextJS, 2023
+      </p>
+      <Link href='https://github.com/witch-factory' className={styles.github}>
+        <Image src='/github-mark.png' alt='Github' width={32} height={32} />
+      </Link>
     </footer>
   );
 }
