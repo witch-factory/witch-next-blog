af6f5fe7e058d540d5d519e3e8433babca231ff2
witch-factory <soakdma37@gmail.com>
2023-05-29 22:59:29 +0900

    푸터 컨테이너 수정

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,12 +8,16 @@ import styles from './styles.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.copyright}>
+      <div className={styles.container}>
+        <div className={styles.inner}>
+          <p className={styles.copyright}>
       © {blogConfig.name}, Built with NextJS, 2023
-      </p>
-      <Link href='https://github.com/witch-factory' className={styles.github}>
-        <Image src='/github-mark.png' alt='Github' width={32} height={32} />
-      </Link>
+          </p>
+          <Link href='https://github.com/witch-factory' className={styles.github}>
+            <Image src='/github-mark.png' alt='Github' width={32} height={32} />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
