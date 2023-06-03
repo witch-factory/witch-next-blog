e0530f73421b3a46dfd552ea464a3adde87bc1d3
witch-factory <soakdma37@gmail.com>
2023-06-03 15:57:59 +0900

    api route runtime 설정

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,5 +1,7 @@
 import { ImageResponse } from '@cloudflare/pages-plugin-vercel-og/api';
 
+export const runtime = 'edge';
+
 export const onRequest = async () => {
   return new ImageResponse(
     <div style={{ display: 'flex' }}>Hello, world!</div>,
