c7fd95216ce3b544fbe3747e548dbbdf21bf3b31
witch-factory <soakdma37@gmail.com>
2023-06-03 12:26:35 +0900

    이미지 최적화, api라우트

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    unoptimized:true,
     remotePatterns: [
       {
         protocol: 'https',
