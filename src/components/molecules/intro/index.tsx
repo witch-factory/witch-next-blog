36094b99f172fb6121c773d5614123c4d073a985
witch-factory <soakdma37@gmail.com>
2023-09-10 19:26:57 +0900

    프로젝트, 포스트 간략 소개용 intro molecule

---
+++ src/components/molecules/intro/index.tsx
@@ -0,0 +1,36 @@
+import Description from '@/components/atoms/description';
+import Tag from '@/components/atoms/tag';
+import Timestamp from '@/components/atoms/timestamp';
+import Title from '@/components/atoms/title';
+
+import styles from './styles.module.css';
+
+export interface IntroProps{
+  title: string;
+  description: string;
+  tags: string[];
+  date?: string;
+}
+
+/* 프로젝트나 아티클의 제목, 설명, 태그를 담는 부분 */
+function Intro(props: IntroProps) {
+  const { title, description, date, tags } = props;
+  return (
+    <div className={styles.container}>
+      <Title heading='h3' size='md'>
+        {title}
+      </Title>
+      <Description>{description}</Description>
+      {tags.length ?
+        <ul className={styles.tagList}>
+          {tags.map((tag: string)=>
+            <Tag key={tag} size='md'>{tag}</Tag>
+          )}
+        </ul> :
+        null}
+      {date ? <Timestamp date={date} /> : null}
+    </div>
+  );
+}
+
+export default Intro;
