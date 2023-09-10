77d63c7b6a7e325fff51a331efcde4e733cbbf5b
witch-factory <soakdma37@gmail.com>
2023-09-10 19:27:45 +0900

    profile organism 재현

---
+++ src/components/organisms/profile/index.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+import Description from '@/components/atoms/description';
+import IntroLink from '@/components/atoms/introLink';
+import Title from '@/components/atoms/title';
+import LinkList from '@/components/templates/linkList';
+import blogConfig from 'blog-config';
+
+import styles from './styles.module.css';
+
+function Profile() {
+  return (
+    <article className={styles.profile}>
+      <Image 
+        className={styles.image} 
+        src={blogConfig.picture} 
+        alt={`${blogConfig.name}의 프로필 사진`} 
+        width={100}
+        height={100}
+        sizes='100px'
+      />
+      <div>
+        <Title heading='h2' size='md'>{blogConfig.name}</Title>
+        <Description>{blogConfig.description}</Description>
+        <LinkList>
+          {Object.entries(blogConfig.social).map(([key, value]) => (
+            <li key={key} className={styles.linkbox}>
+              <IntroLink siteName={key} siteLink={value} />
+            </li>
+          ))}
+        </LinkList>
+
+      </div>
+    </article>
+  );
+}
+
+export default Profile;
