940ccab3649aa3a92b85d29d0bd0f1fa18d06e2a
witch-factory <soakdma37@gmail.com>
2023-09-11 18:49:46 +0900

    resume page app router migration

--- src/app/(doc)/layout.tsx
+++ src/app/(doc)/layout.tsx
@@ -1,10 +1,8 @@
-import PageContainer from '@/components/templates/pageContainer';
-
 function DocLayout({ children }: {children: React.ReactNode}) {
   return (
-    <PageContainer pageType='post'>
+    <>
       {children}
-    </PageContainer>
+    </>
   );
 }
 
