6101753e82b360cb644c70df25d118a440f6bebd
witch-factory <soakdma37@gmail.com>
2023-09-18 00:56:43 +0900

    build config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:'export',
   images:{
     unoptimized:false,
     imageSizes: [64, 384],
     deviceSizes: [768, 1920],
     domains: ['res.cloudinary.com'],
-    minimumCacheTTL: 60*60*24*30,
+    minimumCacheTTL: 60 * 60 * 24 * 30,
   },
   reactStrictMode: false,
   swcMinify:true,
 };
 
-module.exports= (withContentlayer(nextConfig));
+module.exports = (withContentlayer(nextConfig));
