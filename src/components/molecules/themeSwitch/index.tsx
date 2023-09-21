d58ccef3ee87e1bb34c90215fe6aeca18b55d2f2
witch-factory <soakdma37@gmail.com>
2023-09-22 01:16:35 +0900

    핑크 테마 버튼 디자인

--- src/components/molecules/themeSwitch/index.tsx
+++ src/components/molecules/themeSwitch/index.tsx
@@ -4,34 +4,22 @@ import { useTheme } from 'next-themes';
 
 import { getThemeName } from '@/utils/theme';
 
-import PinkIcon from './pinkIcon';
-import SunAndMoonIcon from './sunAndMoon';
-import styles from './theme.module.css';
+import LightDarkToggle from './lightDarkToggle';
+import PinkToggle from './pinkToggle';
+import styles from './styles.module.css';
 
 function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
 
-  const isDark = getThemeName(theme) === 'dark' || getThemeName(theme) === 'darkPink';
-  const toggleTheme = () => setTheme(isDark ? 'light' : 'dark');
-  const togglePinkTheme = () => setTheme(isDark ? 'darkPink' : 'pink');
+  const isDarkOrPink = getThemeName(theme) === 'dark' || getThemeName(theme) === 'pink';
+  const toggleTheme = () => setTheme(isDarkOrPink ? 'light' : 'dark');
+  const togglePinkTheme = () => setTheme(isDarkOrPink ? 'darkPink' : 'pink');
 
   return (
     <div className={styles.container}>
-      <button 
-        className={styles.themeToggle} 
-        title='Toggle Light & Dark' 
-        aria-label='auto'
-        aria-live='polite'
-        onClick={toggleTheme}
-      >
-        <SunAndMoonIcon />
-      </button>
-      <button className={styles.pinkThemeToggle} onClick={togglePinkTheme}>
-        <PinkIcon />
-      </button>
-
+      <LightDarkToggle toggleClick={toggleTheme} />
+      <PinkToggle toggleClick={togglePinkTheme} />
     </div>
-
   );
 }
 
