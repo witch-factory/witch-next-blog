dd6943c2e48a9e3ed39b6110177363794de5f019
witch-factory <soakdma37@gmail.com>
2023-10-04 23:34:44 +0900

    안 쓰는 라이브러리 삭제

--- src/components/viewCounter/index.tsx
+++ src/components/viewCounter/index.tsx
@@ -1,15 +1,8 @@
 import { useEffect } from 'react';
-import useSWR from 'swr';
 
 import styles from './styles.module.css';
 
-const fetcher = async (input: RequestInfo) => {
-  const res: Response = await fetch(input);
-  return await res.json();
-};
-
 function ViewCounter({ slug }: {slug: string}) {
-  const { data } = useSWR(`/api/view/${slug}`, fetcher);
   
   useEffect(() => {
     fetch(`/api/view/${slug}`, {
@@ -19,7 +12,7 @@ function ViewCounter({ slug }: {slug: string}) {
 
   return (
     <div className={styles.counter}>
-      {`조회수 ${data?.view_count ?? '---'}회`}
+      {'조회수 회'}
     </div>
   );
 }
