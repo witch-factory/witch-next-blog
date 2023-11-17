69efb626ff9864ba8133d1ecb5e9ca8af772a69d
witch-factory <soakdma37@gmail.com>
2023-11-18 00:15:52 +0900

    TOC 컴포넌트를 함수에서 컴포넌트로 리팩토링

--- src/components/toc/index.tsx
+++ src/components/toc/index.tsx
@@ -3,26 +3,27 @@ import { headingData } from '@/utils/post';
 import styles from './styles.module.css';
 import TOCLink from './tocLink';
 
-
-function renderContent(nodes: headingData[]) {
+function TOC({ nodes }: { nodes: headingData[] }) {
   return (
-    <ul className={`${styles.list} ${nodes[0].depth - 1 ? '' : styles.list__h1}`}>
+    <ul
+      className={`${styles.list} ${nodes[0].depth - 1 ? '' : styles.list__h1}`}
+    >
       {nodes.map((node: headingData) => (
         <li key={node.data.hProperties.id} className={styles.item}>
           <TOCLink node={node} />
-          {node.children.length > 0 && renderContent(node.children)}
+          {node.children.length > 0 && <TOC nodes={node.children} />}
         </li>
       ))}
     </ul>
   );
 }
 
-function TableOfContents({ nodes }: {nodes: headingData[]}) {
+function TableOfContents({ nodes }: { nodes: headingData[] }) {
   if (!nodes.length) return null;
   return (
     <section className={styles.container}>
       <span className={styles.title}>목차</span>
-      {renderContent(nodes)}
+      <TOC nodes={nodes} />
     </section>
   );
 }
