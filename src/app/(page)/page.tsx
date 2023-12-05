bdef2e4df89e5905d5ef6b448028fefc6f7c2c80
witch-factory <soakdma37@gmail.com>
2023-12-05 13:39:18 +0900

    프로젝트 항목 임시 삭제

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -1,9 +1,6 @@
 import Profile from '@/components/organisms/profile';
-import ProjectCard from '@/components/organisms/projectCard';
 import RecentPosts from '@/components/organisms/recentPosts';
-import ProjectList from '@/components/templates/projectList';
 import { getRecentPosts } from '@/utils/post';
-import blogProjectList from 'blog-project';
 
 function Home() {
   const recentPosts = getRecentPosts();
@@ -11,13 +8,13 @@ function Home() {
   return (
     <>
       <Profile />
-      <ProjectList>
+      {/*<ProjectList>
         {blogProjectList.map((project)=>
           <li key={project.title}>
             <ProjectCard key={project.title} project={project} />
           </li>
         )}
-      </ProjectList>
+        </ProjectList>*/}
       <RecentPosts title='최근에 작성한 글' url='/posts/all' items={recentPosts} />
     </>
   );
