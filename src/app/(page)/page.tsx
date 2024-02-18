908dd52c943ba58f04a23b5d9d4e58d4f521f881
witch-factory <soakdma37@gmail.com>
2024-02-18 22:18:30 +0900

    조회수 카운터 임시삭제

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -1,28 +1,13 @@
-import { Redis } from '@upstash/redis';
-
-
 import Profile from '@/components/organisms/profile';
 import RecentPosts from '@/components/organisms/recentPosts';
-import ViewReporter from '@/components/viewReporter';
 import { getRecentPosts } from '@/utils/post';
-
-const redis = Redis.fromEnv();
-
-// cache revaildate time
-export const revalidate = 60;
-
-const homeSlug = 'witch-blog-homepage';
 
 async function Home() {
   const recentPosts = getRecentPosts();
 
-  const views = await redis.get<number>(['pageviews', 'projects', homeSlug].join(':')) ?? 0;
-
   return (
     <>
-      <ViewReporter slug={homeSlug} />
       <Profile />
-      <h1>{views ?? ''}</h1>
       {/*<ProjectList>
         {blogProjectList.map((project)=>
           <li key={project.title}>
