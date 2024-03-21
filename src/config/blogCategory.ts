7cde4473bcec7da1f8cdea92a36d1a8d628b0c2c
witch-factory <soakdma37@gmail.com>
2024-03-22 01:05:18 +0900

    설정 파일, 타입 폴더 추가

---
+++ src/config/blogCategory.ts
@@ -0,0 +1,6 @@
+import { BlogCategoryType } from '@/types/config';
+
+export const blogCategoryList: BlogCategoryType[] = [
+  { title:'글목록', url:'/posts/all' },
+  { title:'소개', url:'/about' },
+];
