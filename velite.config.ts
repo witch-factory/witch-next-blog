e2a0f3db1119b244be3119036b0b08a26707da71
witch-factory <soakdma37@gmail.com>
2024-03-25 03:04:27 +0900

    toc 플러그인 설정 변경사항 빼먹은 것 추가

--- velite.config.ts
+++ velite.config.ts
@@ -4,13 +4,17 @@ import highlight from 'rehype-highlight';
 import rehypeKatex from 'rehype-katex';
 import rehypePrettyCode from 'rehype-pretty-code';
 import remarkMath from 'remark-math';
+import { UnistNode } from 'unist-util-visit/lib';
 import { defineConfig, defineCollection, s } from 'velite';
+
 
 import { blogConfig } from '@/config/blogConfig';
 import { uploadThumbnail } from '@/utils/cloudinary';
 import { getBase64ImageUrl } from '@/utils/generateBlurPlaceholder';
 import { generateRssFeed } from '@/utils/generateRSSFeed';
+import { generateHeadingTree } from '@/utils/meta/generateHeadingTree';
 
+import headingTree from './src/plugins/heading-tree';
 import { generateThumbnail } from './src/utils/meta/generateThumbnail';
 // `s` is extended from Zod with some custom schemas,
 // you can also import re-exported `z` from `velite` if you don't need these extension schemas.
@@ -31,7 +35,6 @@ const posts = defineCollection({
         gfm:true,
       }), // transform markdown to html
       excerpt:s.excerpt(), // extract excerpt from markdown
-      headingTree:s.toc(),
       metadata:s.metadata(),
       // 썸네일을 일단 만들고...
       thumbnail:s.object({
@@ -39,14 +42,19 @@ const posts = defineCollection({
         cloudinary:s.string().optional(),
         blurURL:s.string().optional(),
       }).optional(),
+      headingTree:s.custom().transform((data, { meta }) => {
+        return generateHeadingTree(meta.mdast as UnistNode);
+      })
+      // toc:s.custom().transform((data, { meta }) => {
+      //   console.log(meta.data);
+      // })
     })
     // more additional fields (computed fields)
     .transform(async (data, { meta }) => {
       if (!meta.mdast) return data;
       const thumbnail = await generateThumbnail(meta, data.title, data.headingTree, data.slug);
-
+      // generateHeadingTree(meta.mdast);
       // add heading tree id
-
       return ({ ...data, url: `/posts/${data.slug}`, thumbnail });
     })
 });
@@ -89,7 +97,7 @@ export default defineConfig({
     clean:true
   },
   markdown:{
-    remarkPlugins:[remarkMath],
+    remarkPlugins:[remarkMath, headingTree],
     rehypePlugins:[
       [rehypePrettyCode, rehypePrettyCodeOptions], 
       rehypeKatex, 
@@ -106,7 +114,6 @@ export default defineConfig({
       // 썸네일이 없는 경우, 현재 post 객체를 그대로 반환합니다.
       if (!post.thumbnail) return post;
 
-      // TODO : 업로드시 파일 앞에 -static-*처럼 "-"가 붙는 문제 해결
       try {
         const results = await uploadThumbnail(post.thumbnail.local);
         post.thumbnail.cloudinary = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_300,f_auto/${results.public_id}`;
