f494dd13a029c2df3480610c226578d53c58b491
witch-factory <soakdma37@gmail.com>
2024-04-06 01:46:19 +0900

    sitemap prioroty 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,26 +7,31 @@ const defaultSiteMap: MetadataRoute.Sitemap = [
   {
     url:blogConfig.url,
     lastModified:new Date(),
+    changeFrequency:'daily',
     priority:1,
   },
   {
     url:blogConfig.url + '/about',
     lastModified:new Date(),
+    changeFrequency:'daily',
     priority:0.8,
   },
   {
     url:blogConfig.url + '/posts/all',
     lastModified:new Date(),
+    changeFrequency:'daily',
     priority:0.8,
   },
 ];
 
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const sitemapFromPosts = postMetadata.map((post)=>{
+  const sitemapFromPosts: MetadataRoute.Sitemap = postMetadata.map((post)=>{
     return {
       url:blogConfig.url + post.url,
       lastModified:new Date(post.date),
+      changeFrequency:'daily',
+      priority:0.7,
     };
   });
   return [...defaultSiteMap, ...sitemapFromPosts];
