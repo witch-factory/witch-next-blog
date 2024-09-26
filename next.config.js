c56275772451b11988f49bc06d16d9cf7244ce71
witch-factory <soakdma37@gmail.com>
2024-09-26 23:18:15 +0900

    velite 버전업, 설정 파일의 plugin 코드 단순화

--- next.config.js
+++ next.config.js
@@ -25,20 +25,15 @@ module.exports = withVanillaExtract(nextConfig);
 
 class VeliteWebpackPlugin {
   static started = false
-  constructor(/** @type {import('velite').Options} */ options = {}) {
-    this.options = options;
-  }
   apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs !!!
+    // executed three times in nextjs
     // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
       if (VeliteWebpackPlugin.started) return;
       VeliteWebpackPlugin.started = true;
       const dev = compiler.options.mode === 'development';
-      this.options.watch = this.options.watch ?? dev;
-      this.options.clean = this.options.clean ?? !dev;
       const { build } = await import('velite');
-      await build(this.options); // start velite
+      await build({ watch: dev, clean: !dev });
     });
   }
 }
