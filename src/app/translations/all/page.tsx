6e010ceb8a5190f660aac069c57f3fd2552729a6
witch-factory <soakdma37@gmail.com>
2024-12-21 02:15:52 +0900

    translation 페이지 파일 생성

---
+++ src/app/translations/all/page.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from 'next';
+
+import { translationsMetadata } from '#site/content';
+import { blogConfig } from '@/config/blogConfig';
+import { PostIntroType } from '@/types/components';
+import AllPostTagFilter from '@/ui/allPostTagFilter';
+import Pagination from '@/ui/pagination';
+import PostList from '@/ui/postList';
+import { getPostsByPage, ITEMS_PER_PAGE, FIRST_PAGE } from '@/utils/post';
+
+
+function PostListPage() {
+  const currentPage = FIRST_PAGE;
+
+  const pagePosts = translationsMetadata;
+  const totalPostNumber = translationsMetadata.length;
+
+  const pagePostsWithThumbnail: PostIntroType[] = pagePosts.map((post) => {
+    const { title, description, date, tags, url } = post;
+    return { title, description, date, tags, url };
+  });
+
+  return (
+    <>
+      <AllPostTagFilter
+        selectedTag={'all'}
+      />
+      <Pagination
+        totalItemNumber={totalPostNumber}
+        currentPage={currentPage}
+        renderPageLink={(page: number) => `/posts/all/${page}`}
+        perPage={ITEMS_PER_PAGE}
+      />
+      <PostList postList={pagePostsWithThumbnail} />
+    </>
+  );
+}
+
+const currentPage = FIRST_PAGE;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `${blogConfig.title}, All Posts ${currentPage} Page`,
+    description: `${blogConfig.title}의 전체 글 중 ${currentPage}페이지 글 목록`,
+    alternates:{
+      canonical:`/posts/all/${currentPage}`,
+    },
+    openGraph:{
+      title: `${blogConfig.title}, All Posts ${currentPage} Page`,
+      description: `${blogConfig.title}의 전체 글 중 ${currentPage}페이지 글 목록`,
+      url:`/posts/all/${currentPage}`,
+    },
+  };
+}
+
+export default PostListPage;
