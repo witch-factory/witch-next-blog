aaf116b3af9942a7d297402a42e38d69d9e298b9
witch-factory <soakdma37@gmail.com>
2024-12-23 19:47:15 +0900

    헤더에 들어갈 게시판 목록에 소개 대신 번역글 목록 페이지 링크 추가

--- src/config/blogCategory.ts
+++ src/config/blogCategory.ts
@@ -2,6 +2,6 @@ import { BlogCategoryType } from '@/types/config';
 
 // TODO: Change the type of the variable to the type of the variable in the snippet
 export const blogCategoryList: BlogCategoryType[] = [
-  { title:'글목록', url:'/posts/all' },
-  { title:'소개', url:'/about' },
+  { title:'작성글', url:'/posts/all' },
+  { title:'번역', url:'/translations/all' },
 ];
