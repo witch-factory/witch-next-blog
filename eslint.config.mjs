d218e7454445515c533d25ec28be8a1979cdad64
witch-factory <soakdma37@gmail.com>
2024-12-27 03:21:36 +0900

    prettier 플러그인 삭제, eslint 설정에서 중복된 것 삭제하고 eslint shared config에서 type check 제거

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslint from "@eslint/js";
-import unusedImports from "eslint-plugin-unused-imports";
-import stylisticJs from "@stylistic/eslint-plugin";
-import tseslint from "typescript-eslint";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,104 +17,86 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+  tseslint.configs.stylistic,
   compat.config({
-    extends: ["next", "next/core-web-vitals"],
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
   }),
   {
-    files: ["src/**/*.{ts,tsx}"],
-    ignores: [".next/*", "node_modules/*", "!src/**/*"],
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['.next/*', 'node_modules/*', '!src/**/*'],
   },
   {
     plugins: {
-      "@stylistic": stylisticJs,
-      "unused-imports": unusedImports,
+      '@stylistic': stylisticJs,
+      'unused-imports': unusedImports,
     },
     rules: {
-      "object-curly-spacing": ["error", "always"],
-      "import/extensions": "off",
-      "import/prefer-default-export": "off",
+      // TODO: import 플러그인 설치/사용
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
 
-      "import/order": [
-        "warn",
+      'import/order': [
+        'warn',
         {
           alphabetize: {
-            order: "asc",
+            order: 'asc',
             caseInsensitive: true,
           },
 
           groups: [
-            "builtin",
-            "external",
-            ["parent", "internal"],
-            "sibling",
-            ["unknown", "index", "object"],
+            'builtin',
+            'external',
+            ['parent', 'internal'],
+            'sibling',
+            ['unknown', 'index', 'object'],
           ],
-
           pathGroups: [
             {
-              pattern: "~/**",
-              group: "internal",
+              pattern: '~/**',
+              group: 'internal',
             },
           ],
 
-          "newlines-between": "always",
+          'newlines-between': 'always',
         },
       ],
 
-      "@stylistic/indent": ["error", 2],
-      "@stylistic/arrow-parens": ["error"],
-      "@stylistic/quotes": [
-        "error",
-        "single",
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/arrow-parens': ['error'],
+      '@stylistic/quotes': [
+        'error',
+        'single',
         {
           allowTemplateLiterals: true,
         },
       ],
-      "@stylistic/jsx-quotes": ["error"],
-      "@stylistic/semi": ["error"],
-      "@stylistic/max-len": ["error", { code: 80, tabWidth: 2 }],
-      "@stylistic/comma-dangle": ["error", "always-multiline"],
-      "@stylistic/keyword-spacing": ["error"],
-      "@stylistic/space-before-blocks": ["error"],
-      "@stylistic/space-infix-ops": ["error"],
-      "@stylistic/member-delimiter-style": [
-        "error",
+      '@stylistic/jsx-quotes': ['error'],
+      '@stylistic/semi': ['error'],
+      '@stylistic/max-len': ['error', { code: 80, tabWidth: 2 }],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/keyword-spacing': ['error'],
+      '@stylistic/space-before-blocks': ['error'],
+      '@stylistic/space-infix-ops': ['error'],
+      '@stylistic/member-delimiter-style': [
+        'error',
         {
           multiline: {
-            delimiter: "comma", // 쉼표 사용
+            delimiter: 'comma', // 쉼표 사용
             requireLast: true,
           },
           singleline: {
-            delimiter: "comma", // 단일 라인에서도 쉼표 사용
+            delimiter: 'comma', // 단일 라인에서도 쉼표 사용
             requireLast: false,
           },
         },
       ],
-      "no-console": [
-        "warn",
+      'no-console': [
+        'warn',
         {
-          allow: ["warn", "error"],
+          allow: ['warn', 'error'],
         },
       ],
-      "react/jsx-filename-extension": [
-        "warn",
-        {
-          extensions: [".tsx"],
-        },
-      ],
-      "react/no-unescaped-entities": "warn",
-      "react/jsx-props-no-spreading": "off",
-      "react/require-default-props": "off",
     },
-  }
+  },
 );
