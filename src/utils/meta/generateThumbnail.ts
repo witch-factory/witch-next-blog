c91709b76a3f974a707d765ca472266000ae3bb7
witch-factory <soakdma37@gmail.com>
2024-12-28 23:44:48 +0900

    번역본 파일을 지원하기 위한 썸네일 생성 함수 개선

--- src/utils/meta/generateThumbnail.ts
+++ src/utils/meta/generateThumbnail.ts
@@ -1,8 +1,11 @@
+import path from 'node:path';
+
 import { Root as Mdast } from 'mdast';
 import { visit } from 'unist-util-visit';
 import { isRelativePath, processAsset, ZodMeta } from 'velite';
 
 import { blogConfig } from '@/config/blogConfig';
+import { Language } from '@/types/i18n';
 
 // 모든 이미지 뽑아내기
 function extractImgSrc(tree: Mdast) {
@@ -19,13 +22,25 @@ async function processImageForThumbnail(imageURL: string, meta: ZodMeta) {
   return processedImage.src;
 }
 
-export async function generateThumbnailURL(meta: ZodMeta, title: string) {
+// 언어별 이미지 경로 업데이트
+function updateImagePathForLanguage(imageURL: string, meta: ZodMeta, lang: Language): string {
+  if (lang === 'ko') return imageURL;
+
+  const articleSlugPath = path.basename(path.dirname(meta.path));
+  const updatedDir = `../../posts`;
+  const fileName = imageURL.replace('./', '');
+
+  return `${updatedDir}/${articleSlugPath}/${fileName}`;
+}
+
+export async function generateThumbnailURL(meta: ZodMeta, title: string, lang: Language = 'ko') {
   // source of the images
   if (!meta.mdast) return '/witch-new-hat.png';
   const images = extractImgSrc(meta.mdast);
   if (images.length > 0) {
-    const imageURL = images[0];
+    const imageURL = updateImagePathForLanguage(images[0], meta, lang);
 
+    // console.log('이미지 경로 ', imageURL);
     // 상대 경로 이미지인 경우 processAsset 함수로 처리
     return isRelativePath(imageURL)
       ? processImageForThumbnail(imageURL, meta)
@@ -33,6 +48,6 @@ export async function generateThumbnailURL(meta: ZodMeta, title: string) {
   }
   else {
     // vercel/og를 이용한 open graph 이미지 생성
-    return `${blogConfig.url}/api/og?title=${title}`;
+    return `${blogConfig.url}/api/og?title=${encodeURIComponent(title)}`;
   }
 }
