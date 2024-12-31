81e7469fd9f687537798642cc98dad71922972ba
witch-factory <soakdma37@gmail.com>
2024-12-31 21:26:17 +0900

    블로그 프로필 컴포넌트가 언어 props를 받도록 변경

--- src/ui/profile/index.tsx
+++ src/ui/profile/index.tsx
@@ -1,27 +1,30 @@
 import Image from 'next/image';
 
 import { blogConfig } from '@/config/blogConfig';
+import { Language } from '@/types/i18n';
 
 import ProfileLinkList from './linkList';
 import * as styles from './styles.css';
 
-function Profile() {
+function Profile({ lang }: { lang: Language }) {
   return (
     <article className={styles.profile}>
       <Image
         className={styles.image}
-        src={blogConfig.picture}
-        alt={`${blogConfig.name}의 프로필 사진`}
+        src={blogConfig[lang].picture}
+        alt={
+          lang === 'ko' ? `${blogConfig.ko.name}의 프로필 사진` : `${blogConfig.en.name} profile picture`
+        }
         width={100}
         height={100}
-        placeholder={blogConfig.pictureBlur ? 'blur' : 'empty'}
-        blurDataURL={blogConfig.pictureBlur}
+        placeholder={blogConfig[lang].pictureBlur ? 'blur' : 'empty'}
+        blurDataURL={blogConfig[lang].pictureBlur}
         sizes="100px"
       />
       <div>
-        <h2 className={styles.title}>{blogConfig.name}</h2>
-        <p className={styles.description}>{blogConfig.description}</p>
-        <ProfileLinkList linkList={Object.entries(blogConfig.social).map((value) => {
+        <h2 className={styles.title}>{blogConfig[lang].name}</h2>
+        <p className={styles.description}>{blogConfig[lang].description}</p>
+        <ProfileLinkList linkList={Object.entries(blogConfig[lang].social).map((value) => {
           return { siteName: value[0], siteLink: value[1] };
         })}
         />
