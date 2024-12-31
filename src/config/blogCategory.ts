75890be2293a04385ec279ae3c5f90338e59386f
witch-factory <soakdma37@gmail.com>
2025-01-01 01:26:31 +0900

    헤더의 메뉴 항목이 언어에 따라 바뀌도록 설정

--- src/config/blogCategory.ts
+++ src/config/blogCategory.ts
@@ -1,6 +1,16 @@
 import { BlogCategoryType } from '@/types/config';
 
-export const blogCategoryList: BlogCategoryType[] = [
+const blogCategoryList: BlogCategoryType[] = [
   { title: '작성글', url: '/posts/all' },
   { title: '번역', url: '/translations/all' },
 ];
+
+const enBlogCategoryList: BlogCategoryType[] = [
+  { title: 'Posts', url: '/en/posts/all' },
+  { title: 'Transl.', url: '/en/translations/all' },
+];
+
+export const blogCategory = {
+  ko: blogCategoryList,
+  en: enBlogCategoryList,
+};
