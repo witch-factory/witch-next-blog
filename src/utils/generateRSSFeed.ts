7effc063b5072199649bf012aff47639623943f5
witch-factory <soakdma37@gmail.com>
2025-01-01 21:48:27 +0900

    다국어 글은 피드에 추가되도록 설정

--- src/utils/generateRSSFeed.ts
+++ src/utils/generateRSSFeed.ts
@@ -3,17 +3,19 @@ import { writeFileSync } from 'fs';
 import { Feed } from 'feed';
 
 import { blogConfig } from '@/config/blogConfig';
+import { locales } from '@/types/i18n';
 
 import { getSortedPosts } from './post';
 
+// TODO: Add feed for each language
 export function generateRssFeed() {
-  const blogUrl = blogConfig.url;
+  const blogUrl = blogConfig.ko.url;
 
   const feed = new Feed({
     id: blogUrl,
-    title: blogConfig.title,
-    description: blogConfig.description,
-    link: blogConfig.url,
+    title: blogConfig.ko.title,
+    description: blogConfig.ko.description,
+    link: blogConfig.ko.url,
     feedLinks: {
       rss2: `${blogUrl}/feed.xml`,
       json: `${blogUrl}/feed.json`,
@@ -23,14 +25,16 @@ export function generateRssFeed() {
     copyright: '',
   });
 
-  getSortedPosts().map((post) => {
-    feed.addItem({
-      title: post.title,
-      description: post.description,
-      link: `${blogUrl}${post.url}`,
-      date: new Date(post.date),
+  for (const lang of locales) {
+    getSortedPosts(lang).map((post) => {
+      feed.addItem({
+        title: post.title,
+        description: post.description,
+        link: `${blogConfig[lang].url}${post.url}`,
+        date: new Date(post.date),
+      });
     });
-  });
+  }
 
   writeFileSync('./public/feed.xml', feed.rss2(), { encoding: 'utf-8' });
   writeFileSync('./public/atom.xml', feed.atom1(), { encoding: 'utf-8' });
