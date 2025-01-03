8854dea097d8293c0ed7eeac95e50e8749d6d68e
witch-factory <soakdma37@gmail.com>
2025-01-04 00:35:51 +0900

    전반적으로 seo 설정 보강, 트위터 메타데이터 추가

--- src/utils/generatePostsPageMetadata.ts
+++ src/utils/generatePostsPageMetadata.ts
@@ -5,31 +5,79 @@ import { Language } from '@/types/i18n';
 
 // 언어가 추가될 때마다 새로운 메타데이터 객체를 추가
 export function generatePostsPageMetadata(lang: Language, currentPage: number, tag: string): Metadata {
+  const koTitle = `${blogConfig[lang].title}, ${tag === 'all' ? '모든' : tag} 글 ${currentPage}페이지`;
+  const koDescription = `${blogConfig[lang].title}의 ${tag === 'all' ? '전체' : tag} 글 목록 ${currentPage}페이지`;
+
+  const enTitle = `${blogConfig[lang].title}, ${tag === 'all' ? 'All' : tag} Posts ${currentPage} Page`;
+  const enDescription = `${blogConfig[lang].title}, ${tag === 'all' ? 'All' : tag} Posts ${currentPage} Page`;
+
   switch (lang) {
     case 'ko':
       return {
-        title: `${blogConfig[lang].title}, ${tag === 'all' ? '모든' : tag} 글 ${currentPage}페이지`,
-        description: `${blogConfig[lang].title}의 ${tag === 'all' ? '전체' : tag} 글 목록 ${currentPage}페이지`,
+        title: koTitle,
+        description: koDescription,
         alternates: {
           canonical: `/posts/${tag === 'all' ? '' : 'tag/'}${tag}/${currentPage}`,
         },
         openGraph: {
-          title: `${blogConfig[lang].title}, ${tag === 'all' ? '모든' : tag} 글 ${currentPage}페이지`,
-          description: `${blogConfig[lang].title}의 ${tag === 'all' ? '전체' : tag} 글 목록 ${currentPage}페이지`,
+          type: 'website',
+          locale: 'ko_KR',
+          title: koTitle,
+          description: koDescription,
           url: `/posts/${tag}/${currentPage}`,
+          images: [
+            {
+              url: blogConfig.ko.thumbnail,
+              alt: `${blogConfig.ko.name} 프로필 사진`,
+            },
+          ],
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@witch_front',
+          creator: '@witch_front',
+          title: koTitle,
+          description: koDescription,
+          images: [
+            {
+              url: blogConfig.ko.thumbnail,
+              alt: `${blogConfig.ko.name} 프로필 사진`,
+            },
+          ],
         },
       };
     case 'en':
       return {
-        title: `${blogConfig[lang].title}, ${tag === 'all' ? 'All' : tag} Posts ${currentPage} Page`,
-        description: `${blogConfig[lang].title}, ${tag === 'all' ? 'All' : tag} Posts ${currentPage} Page`,
+        title: enTitle,
+        description: enDescription,
         alternates: {
-          canonical: `/posts/${tag === 'all' ? '' : 'tag/'}${tag}/${currentPage}`,
+          canonical: `/en/posts/${tag === 'all' ? '' : 'tag/'}${tag}/${currentPage}`,
         },
         openGraph: {
-          title: `${blogConfig[lang].title}, All Posts ${currentPage} Page`,
-          description: `${blogConfig[lang].title}, All Posts ${currentPage} Page`,
-          url: `/posts/${tag}/${currentPage}`,
+          type: 'website',
+          locale: 'en_US',
+          title: enTitle,
+          description: enDescription,
+          url: `/en/posts/${tag}/${currentPage}`,
+          images: [
+            {
+              url: blogConfig.ko.thumbnail,
+              alt: `${blogConfig.ko.name} 프로필 사진`,
+            },
+          ],
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@witch_front',
+          creator: '@witch_front',
+          title: enTitle,
+          description: enDescription,
+          images: [
+            {
+              url: blogConfig.en.thumbnail,
+              alt: `${blogConfig.en.name} profile picture`,
+            },
+          ],
         },
       };
   }
