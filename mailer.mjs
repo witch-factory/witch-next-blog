4323c923d2b597c59bbec6cfb7fb3fe0e091d759
witch-factory <soakdma37@gmail.com>
2025-01-03 17:41:50 +0900

    mailer를 git diff 기반으로 변경

--- mailer.mjs
+++ mailer.mjs
@@ -1,12 +1,12 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
-import path from 'path';
 import readline from 'readline';
 import * as nodemailer from 'nodemailer';
 
-function getGitStatus(directory) {
+function getGitDiff(directory) {
   try {
-    const gitStatus = execSync(`git status --porcelain ${directory}`).toString();
+    // 이전 커밋과 현재 커밋 사이의 변경 사항을 확인
+    const gitStatus = execSync(`git diff --name-status HEAD^ HEAD ${directory}`).toString();
     return gitStatus;
   }
   catch (error) {
@@ -32,72 +32,38 @@ async function readFileLines(filePath, numLines = 4) {
   return lines.join('\n');
 }
 
-// listChangedFiles에서 생기는 {status, filePath}를 받아서 처리
-function resolveFiles(files) {
-  const resolvedFiles = files.map((file) => {
-    const filePath = file.filePath;
-    const stats = fs.statSync(filePath);
-    if (stats.isDirectory()) {
-      const indexPath = path.join(filePath, 'index.md');
-      if (fs.existsSync(indexPath)) {
-        return indexPath;
-      }
-      console.log('index.md 파일이 해당 경로에 없습니다: ', filePath);
-      return null;
-    }
-    else {
-      return filePath;
-    }
-  }).filter(Boolean); // null 값을 제거
-
-  return resolvedFiles;
-}
-
-async function listChangedFiles(directory) {
-  const gitStatus = getGitStatus(directory);
-  if (!gitStatus) {
-    console.log('변경 사항이 없습니다.');
-    return [];
-  }
-
-  const files = gitStatus.split('\n').map((line) => {
-    // 정규식을 써서 더 강력한 공백 매칭도 가능하지만 여기서는 간단히 했다
-    const [status, filePath] = line.trim().split(' ');
+async function getChangedFilesContent(diffOutput) {
+  const fileChanges = diffOutput.split('\n').map((line) => {
+    const [status, filePath] = line.trim().split('\t');
     return { status, filePath };
   });
-
-  const newFiles = resolveFiles(files.filter((file) => file.status === '??'));
-  const modifiedFiles = resolveFiles(files.filter((file) => file.status === 'M'));
-
-  // 파일 내용 읽기
-  const allFiles = [...newFiles, ...modifiedFiles];
+  console.log('File Changes:', fileChanges);
 
   const fileContents = await Promise.all(
-    allFiles.map(async (filePath) => {
-      const content = await readFileLines(filePath);
-      return content ? { filePath, content: content.split('\n').slice(1) } : null;
-    }),
-  );
+    fileChanges.map(async ({ status, filePath }) => {
+      // 파일이 .md 파일이 아니면 무시
+      if (filePath && !filePath.endsWith('.md')) {
+        return null;
+      }
+      switch (status) {
+        case 'D':
+          return { status, filePath, content: null };
+        case 'A':
+        case 'M':
+          if (fs.existsSync(filePath)) {
+            const content = (await readFileLines(filePath)).split('\n').slice(1);
+            return { status, filePath, content };
+          }
+          else {
+            console.warn(`File not found: ${filePath}`);
+            return null;
+          }
+      }
+    },
+    ));
 
-  // null 값을 제거하고 반환
   return fileContents.filter(Boolean);
 }
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true for port 465, false for other ports
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-
-const mailOptions = {
-  from: 'soakdma37@gmail.com',
-  to: 'soakdma37@gmail.com',
-  subject: `${new Date().toLocaleDateString()} 블로그 글 변경 알림`,
-};
 
 async function sendMail(changedFiles) {
   if (!changedFiles || changedFiles.length === 0) {
@@ -105,18 +71,50 @@ async function sendMail(changedFiles) {
     return;
   }
 
+  console.log(process.env.MAIL_PASS);
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: 'soakdma37@gmail.com',
+    to: 'soakdma37@gmail.com',
+    subject: `${new Date().toLocaleDateString('ko-KR')} 블로그 글 변경 알림`,
+  };
+
   const emailContent = `
     <h1>새로 생성되거나 변경된 글이 있습니다.</h1>
     <p>확인하고 필요하면 번역해 주세요.</p>
     <hr>
     ${changedFiles
       .map(
-        ({ filePath, content }) => `
-      <div>
-        <h3>${filePath}</h3>
-        <pre>${content.join('<br>')}</pre>
-      </div>
-    `,
+        ({ status, filePath, content }) => {
+          const statusText
+          = status === 'M'
+            ? '파일 수정'
+            : status === 'A'
+              ? '파일 추가'
+              : status === 'D'
+                ? '파일 삭제'
+                : '알 수 없는 변경';
+
+          return `
+                <div>
+                  <h3>${statusText}: ${filePath}</h3>
+                  ${
+                    content
+                      ? `<pre>${content.join('<br>')}</pre>`
+                      : '<p>No content available (deleted file).</p>'
+                  }
+                </div>`;
+        },
       )
       .join('<hr>')}
   `;
@@ -135,7 +133,15 @@ async function sendMail(changedFiles) {
 
 (async () => {
   const targetDirectory = 'content/posts';
-  const fileContents = await listChangedFiles(targetDirectory);
-  console.log('Changed Files Content:', fileContents);
-  await sendMail(fileContents);
+  const gitDiff = getGitDiff(targetDirectory);
+
+  if (!gitDiff) {
+    console.log('변경된 파일이 없습니다.');
+    return;
+  }
+
+  const changedFiles = await getChangedFilesContent(gitDiff, targetDirectory);
+  console.log('Changed Files:', changedFiles);
+
+  await sendMail(changedFiles);
 })();
