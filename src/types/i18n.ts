b831d4fe952126ba8fdcfb926085d062923e3640
witch-factory <soakdma37@gmail.com>
2025-01-19 23:09:36 +0900

    i18n을 위한  객체 추가

--- src/types/i18n.ts
+++ src/types/i18n.ts
@@ -1,3 +1,10 @@
+export const i18n = {
+  defaultLocale: 'ko',
+  locales: ['ko', 'en'],
+} as const;
+
 export const locales = ['ko', 'en'] as const; // 지원하는 언어
 
 export type Language = typeof locales[number];
+
+export type Locale = (typeof i18n)['locales'][number];
