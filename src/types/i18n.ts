4b42a35f48b7a8bfbc4a1a17bf5883254c44fa6c
witch-factory <soakdma37@gmail.com>
2025-01-24 01:26:08 +0900

    선언 순서 통일

--- src/types/i18n.ts
+++ src/types/i18n.ts
@@ -3,6 +3,6 @@ export const i18n = {
   locales: ['ko', 'en'],
 } as const;
 
-export const LOCALE_COOKIE_NAME = 'NEXT_LOCALE';
-
 export type Locale = (typeof i18n)['locales'][number];
+
+export const LOCALE_COOKIE_NAME = 'NEXT_LOCALE';
