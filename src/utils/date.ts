2472475aa37ee6b4ae45250d9531ea13bfd84f35
witch-factory <soakdma37@gmail.com>
2025-01-27 00:37:58 +0900

    날짜 포맷 형식 변경

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,11 +4,10 @@ export const toISODate = (date: Date) => {
   return date.toISOString().split('T')[0];
 };
 
-// TODO: 언어에 따른 날짜 포매팅
 export const formatDate = (date: Date, lang: Locale) => {
   const formatter = new Intl.DateTimeFormat(lang, {
     year: 'numeric',
-    month: 'numeric',
+    month: lang === 'ko' ? 'long' : 'short',
     day: 'numeric',
   });
 
