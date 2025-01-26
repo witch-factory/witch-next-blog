41a7e7ecbc022926f0fa8c65bf6e68c3c38ae7fa
witch-factory <soakdma37@gmail.com>
2025-01-27 00:18:17 +0900

    날짜 표기 유틸함수를 국제화

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,16 @@
+import { Locale } from '@/types/i18n';
+
 export const toISODate = (date: Date) => {
   return date.toISOString().split('T')[0];
 };
 
-export const formatDate = (date: Date) => {
-  const year = date.getFullYear().toString();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}. ${month}. ${day}`;
+// TODO: 언어에 따른 날짜 포매팅
+export const formatDate = (date: Date, lang: Locale) => {
+  const formatter = new Intl.DateTimeFormat(lang, {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+
+  return formatter.format(date);
 };
