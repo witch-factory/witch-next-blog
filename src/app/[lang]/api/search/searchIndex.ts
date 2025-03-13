e494ecdda5528b868035acc005d5723439eea9bb
witch-factory <soakdma37@gmail.com>
2025-03-13 23:55:35 +0900

    검색을 위한 라우트 핸들러 작성

--- src/app/[lang]/api/search/searchIndex.ts
+++ src/app/[lang]/api/search/searchIndex.ts
@@ -1,16 +1,30 @@
 import FlexSearch from 'flexsearch';
 
-import { enPosts, posts } from '#site/content';
+import { enPosts, posts, translations } from '#site/content';
+import { searchProperty, sortByDate } from '@/utils/content/helper';
 
-const koIndex = new FlexSearch.Index('performance');
-const enIndex = new FlexSearch.Index('performance');
-
-posts.forEach((post) => {
-  koIndex.add(post.title, JSON.stringify(post));
+const koIndex = new FlexSearch.Index({
+  charset: 'latin:simple',
+  language: 'ko',
+  tokenize: 'reverse',
+});
+const enIndex = new FlexSearch.Index({
+  charset: 'latin:simple',
+  language: 'en',
+  tokenize: 'reverse',
 });
 
-enPosts.forEach((post) => {
-  enIndex.add(post.title, JSON.stringify(post));
+const allKoPosts = sortByDate([...posts, ...translations]);
+const allEnPosts = sortByDate([...enPosts, ...translations]);
+
+allKoPosts.forEach((post) => {
+  const metadata = searchProperty(post);
+  koIndex.add(JSON.stringify(metadata), JSON.stringify(post));
+});
+
+allEnPosts.forEach((post) => {
+  const metadata = searchProperty(post);
+  enIndex.add(JSON.stringify(metadata), JSON.stringify(post));
 });
 
 export { koIndex, enIndex };
