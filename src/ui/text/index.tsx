3ac49fff11e153b6951c695050d241cf63dda873
witch-factory <soakdma37@gmail.com>
2025-04-21 14:47:32 +0900

    text 컴포넌트가 한정된 props만 받게 설정

--- src/ui/text/index.tsx
+++ src/ui/text/index.tsx
@@ -1,3 +1,5 @@
+import { pickProps } from '@/utils/pickProps';
+
 import { text, TextVariants } from './styles.css';
 
 type TextProps = {
@@ -10,10 +12,11 @@ export default function Text({
   children,
   ...props
 }: TextProps) {
+  const textProps = pickProps(props, ['size', 'color', 'weight', 'decoration']);
   return (
     <Component
       {...props}
-      className={text(props)}
+      className={text(textProps)} // Use the text recipe to apply styles
     >
       {children}
     </Component>
