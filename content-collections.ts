77298678edbee4fd9e28c115bba2969d4bb439c4
SungHyun Kim <soakdma37@gmail.com>
2026-04-11 15:34:43 +0900

    slug 변환 추가

--- content-collections.ts
+++ content-collections.ts
@@ -1,30 +1,90 @@
+import path from 'node:path';
+
 import { defineCollection, defineConfig } from '@content-collections/core';
 import { compileMarkdown } from '@content-collections/markdown';
 import { z } from 'zod';
 
-// for more information on configuration, visit:
-// https://www.content-collections.dev/docs/configuration
+import { Locale } from '@/constants/i18n';
+
+type DocumentKind = Locale | 'translation';
+
+function getSlugFromFilePath(filePath: string) {
+  return path.basename(path.dirname(filePath));
+}
+
+const createUrl = (slug: string, kind: DocumentKind) => {
+  switch (kind) {
+    case 'ko':
+    case 'en':
+      return `/posts/${slug}`;
+    case 'translation':
+      return `/translations/${slug}`;
+    default:
+        kind satisfies never;
+      return '';
+  }
+};
+
+const basePostSchema = z.object({
+  title: z.string().max(99),
+  date: z.iso.datetime(),
+  description: z.string().max(200),
+  content: z.string(),
+  tags: z.array(z.string()).default([]),
+});
 
 const posts = defineCollection({
   name: 'posts',
   directory: 'content/posts',
   include: '**/*.md',
-  schema: z.object({
-    title: z.string().max(99),
-    description: z.string().max(200),
-    date: z.iso.datetime(),
-    tags: z.array(z.string()).default([]),
-    content: z.string(),
-  }),
+  schema: basePostSchema,
   transform: async (document, context) => {
     const html = await compileMarkdown(context, document);
+    const slug = getSlugFromFilePath(document._meta.filePath);
+
     return {
       ...document,
+      slug,
+      url: createUrl(slug, 'ko'),
+      html,
+    };
+  },
+});
+
+const enPosts = defineCollection({
+  name: 'enPosts',
+  directory: 'content/en-posts',
+  include: '**/*.md',
+  schema: basePostSchema,
+  transform: async (document, context) => {
+    const html = await compileMarkdown(context, document);
+    const slug = getSlugFromFilePath(document._meta.filePath);
+    return {
+      ...document,
+      slug,
+      url: createUrl(slug, 'en'),
+      html,
+    };
+  },
+});
+
+const translations = defineCollection({
+  name: 'translations',
+  directory: 'content/translations',
+  include: '**/*.md',
+  schema: basePostSchema,
+  transform: async (document, context) => {
+    const html = await compileMarkdown(context, document);
+    const slug = getSlugFromFilePath(document._meta.filePath);
+    return {
+      ...document,
+      slug,
+      url: createUrl(slug, 'translation'),
       html,
     };
   },
 });
 
 export default defineConfig({
-  content: [posts],
+  content: [posts, enPosts, translations],
 });
